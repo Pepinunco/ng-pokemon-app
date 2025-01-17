3f618cfdf7ddb160082157117197b743e59759e7
Pepinunco <jeanpac31@gmail.com>
2025-01-17 10:38:27 +0100

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { BorderCardDirective } from './border-card.directive';
+import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
+import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
+import { RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
+import { routes } from './app.routes';
 
 @NgModule({
-  declarations: [AppComponent], // Declare the component here
-  imports: [BrowserModule],     // Import necessary Angular modules
+  declarations: [
+    AppComponent,
+    BorderCardDirective,
+    PokemonTypeColorPipe,
+    ListPokemonComponent,
+    DetailPokemonComponent
+  ], // Declare the component here
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],     // Import necessary Angular modules
   bootstrap: [AppComponent],    // Bootstrap the root component
 })
 export class AppModule {}
---
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-pokemon',
+  standalone: false,
+  templateUrl: './detail-pokemon.component.html',
+  styles: ``
+})
+export class DetailPokemonComponent {
+
+}
