64d988fc233dd1e984fd64e3d227a953a69690d1
Pepinunco <jeanpac31@gmail.com>
2025-01-21 15:16:20 +0100

    things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { routes } from './app.routes';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     PokemonModule,
     RouterModule.forRoot(routes),
   ],     // Import necessary Angular modules
