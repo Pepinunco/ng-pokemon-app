2a7cf88046333979bec56bfeaee6d1e6df9a0c6f
Pepinunco <jeanpac31@gmail.com>
2025-01-21 14:55:23 +0100

    routes + edit + other stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { BorderCardDirective } from './border-card.directive';
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
-import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
-import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
 import { RouterModule } from '@angular/router';
-import { Routes } from '@angular/router';
 import { routes } from './app.routes';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BorderCardDirective,
-    PokemonTypeColorPipe,
-    ListPokemonComponent,
-    DetailPokemonComponent
+    PageNotFoundComponent
   ], // Declare the component here
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    FormsModule,
+    PokemonModule,
+    RouterModule.forRoot(routes),
   ],     // Import necessary Angular modules
   bootstrap: [AppComponent],    // Bootstrap the root component
 })
---
+++ src/app/pokemon/list-pokemon/list-pokemon.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from '../mock-pokemon-list';
+import { Pokemon } from '../pokemon';
+import { Router } from '@angular/router';
+import { PokemonService } from '../pokemon.service';
+
+@Component({
+  selector: 'app-list-pokemon',
+  templateUrl: './list-pokemon.component.html',
+})
+export class ListPokemonComponent implements OnInit {
+  pokemonList: Pokemon[] = POKEMONS;
+
+  constructor(
+    private router: Router,
+    private pokemonService: PokemonService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.pokemonList = this.pokemonService.getPokemonList();
+  }
+
+  goToPokemon(pokemon: Pokemon) {
+    this.router.navigate(['/pokemon', pokemon.id]);
+  }
+
+}
